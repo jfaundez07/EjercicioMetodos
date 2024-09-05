0ca5fdf1dc4f77a14dae80143b191521f04698f0
jfaundez07 <j.faundez07@ufromail.cl>
2024-09-04 21:46:47 -0400

    setUp: Se plantea modelo base de la solucion

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,74 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        setUp();
+    }
+
+    public static void setUp() {
+
+        String[][] infoPlantas = new String[9][1]; //9 filas y 2 columnas
+
+        menu(infoPlantas);
+
+    }
+
+    public static void menu(String[][] plantas) {
+
+        // implementar mostrar opciones del menu
+
+        switch (opcion) {
+            case 0:
+                registrarPlanta(plantas, Rosa);
+            
+            /* 
+                .
+                .
+                .
+            */
+
+            case 4:
+                diaSiguiente(plantas);
+        }
+
+        
+    }
+
+    public static void registrarPlanta(String[][] plantas, String nombre) {
+
+        for (int i = 0; i < plantas.length; i++) {
+            if (plantas[i][0] == null) {
+
+                // Que esta recorriendo en este bucle for?
+
+                break;
+            }
+        }
+    }
+
+    public static void mostrarPlantas(String[][] plantas) {
+
+        for (int i = 0; i < plantas.length; i++) {
+           
+        }
+    }
+
+    public static void buscarPlanta(String[][] plantas, String nombre) {
+
+        for (int i = 0; i < plantas.length; i++) {
+            if ( ) {
+                System.out.println( /* Muestre la informacion de la planta*/ );
+            }
+        }
+    }
+
+    public static void regarPlanta(String[][] plantas, String nombre) {
+
+    }
+
+    public static void diaSiguiente(String[][] plantas) {
+
     }
 }
