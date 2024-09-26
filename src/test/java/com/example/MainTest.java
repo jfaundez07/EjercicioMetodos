9c97a4a5b88f3c813ec3c741eda982383a7f7ee0
jfaundez07 <j.faundez07@ufromail.cl>
2024-09-25 23:01:07 -0300

    update(pom.xml): se añade la dependencia de JUnit 5 update(test): se genera el archivo para implementar pruebas unitarias

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void registrarPlanta() {
+	}
+
+	@Test
+	void buscarPlanta() {
+	}
+
+	@Test
+	void regarPlanta() {
+	}
+
+	@Test
+	void diaSiguiente() {
+	}
+}
